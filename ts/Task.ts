dbfcf5c2982e4736ed066474dbf13d1e9bebaee5
ryosuketter <r.shimizu.yale@gmail.com>
2023-01-21 09:08:09 +0900

    タスクのステータスを定義

--- ts/Task.ts
+++ ts/Task.ts
@@ -1,10 +1,18 @@
 import { v4 as uuid } from "uuid"
+
+enum Status {
+  Todo = "TODO",
+  Doing = "DOING",
+  Done = "DONE",
+}
 export class Task {
   readonly id
   title
+  status
 
   constructor(properties: { title: string }) {
     this.id = uuid()
     this.title = properties.title
+    this.status = Status.Todo
   }
 }

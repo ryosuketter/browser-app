464e2ad36cfe262589437b09953dbd0855f6cbfd
ryosuketter <r.shimizu.yale@gmail.com>
2023-01-21 11:16:12 +0900

    タスククラスのリファクタ

--- ts/Task.ts
+++ ts/Task.ts
@@ -1,10 +1,13 @@
 import { v4 as uuid } from "uuid"
 
-enum Status {
-  Todo = "TODO",
-  Doing = "DOING",
-  Done = "DONE",
-}
+export const statusMap = {
+  todo: "TODO",
+  doing: "DOING",
+  done: "DONE",
+} as const
+
+export type Status = keyof typeof statusMap
+
 export class Task {
   readonly id
   title
@@ -13,6 +16,6 @@ export class Task {
   constructor(properties: { title: string }) {
     this.id = uuid()
     this.title = properties.title
-    this.status = Status.Todo
+    this.status = statusMap.todo
   }
 }

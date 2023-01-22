1627dcd6490b2f3fbbd6a24dd7d94dde46a29b7e
ryosuketter <r.shimizu.yale@gmail.com>
2023-01-22 14:32:09 +0900

    タスク削除ボタンのイベント登録

--- ts/index.ts
+++ ts/index.ts
@@ -32,9 +32,22 @@ class Application {
     this.taskCollection.add(task)
     console.log("this.taskCollection", this.taskCollection)
 
-    this.taskRenderer.append(task)
+    const { deleteButtonEl } = this.taskRenderer.append(task)
+
+    this.eventListener.add(task.id, "click", deleteButtonEl, () =>
+      this.handleClickDeleteTask(task)
+    )
 
     titleInput.value = ""
+  }
+
+  private handleClickDeleteTask = (task: Task) => {
+    if (!window.confirm(`「${task.title}」を削除してよろしいですか？`)) return
+    console.log(task)
+
+    // this.eventListener.remove(task.id)
+    // this.taskCollection.delete(task)
+    // this.taskRenderer.remove(task)
   }
 }
 

7bcf8a04fd1ab0a1013db067a95ab70760933d77
ryosuketter <r.shimizu.yale@gmail.com>
2023-02-05 15:31:46 +0900

    refactor: イベントリスナー add ロジック リファクタ

--- ts/index.ts
+++ ts/index.ts
@@ -20,19 +20,16 @@ class Application {
     ) as HTMLElement
 
     taskItems.forEach(({ task, deleteButtonEl }) => {
-      this.eventListener.add(task.id, "click", deleteButtonEl, () =>
-        this.handleClickDeleteTask(task)
+      this.eventListener.add(
+        "click",
+        deleteButtonEl,
+        () => this.handleClickDeleteTask(task),
+        task.id
       )
     })
 
+    this.eventListener.add("submit", createForm, this.handleSubmit)
     this.eventListener.add(
-      "submit-handler",
-      "submit",
-      createForm,
-      this.handleSubmit
-    )
-    this.eventListener.add(
-      "click-handler",
       "click",
       deleteAllDoneTaskButton,
       this.handleClickDeleteAllDoneTasks
@@ -54,8 +51,11 @@ class Application {
 
     const { deleteButtonEl } = this.taskRenderer.append(task)
 
-    this.eventListener.add(task.id, "click", deleteButtonEl, () =>
-      this.handleClickDeleteTask(task)
+    this.eventListener.add(
+      "click",
+      deleteButtonEl,
+      () => this.handleClickDeleteTask(task),
+      task.id
     )
 
     titleInput.value = ""
